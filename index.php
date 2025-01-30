82f39fce0ca8fbe72fa83cfb5549aff8cd2bed58
Tilak R <tilakr2203@gmail.com>
2025-01-31 00:28:32 +0530

    Sample commit

---
+++ index.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AutoRevive | Car Rental System</title>
+
+    <link rel="shortcut icon" type="image/png" href="assets/img/P.png">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+
+    <link rel="stylesheet" href="assets/css/user.css">
+
+    <link rel="stylesheet" href="assets/w3css/w3.css">
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation" style="color: black">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                    </button>
+                <a class="navbar-brand page-scroll" href="index.php">
+                <b>AutoRevive</b></a>
+            </div>
+
+            
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php"><b>Home</b></a>
+                    </li>
+                    <li>
+                        <a href="clientlogin.php"><b>Employee</b></a>
+                    </li>
+                    <li>
+                    <a href="customerlogin.php"><b>Customer</b></a>
+                    </li>
+                    <li>
+                        <a href="faq/index.php"><b>FAQ</b></a>
+                    </li>
+                </ul>
+            </div>
+                        </div>
+
+
+    </nav>
+    <div class="bgimg-1">
+        <header class="intro">
+            <div class="intro-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2">
+                            <h1 class="brand-heading" style="color: black">Online Car Rental Service</h1>
+                            <p class="intro-text">
+                            Easy way to rent your dream car at an affordable price!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+
+    <div id="sec2" style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+        <h3 style="text-align:center;"><b>Available Cars</b></h3>
+<br>
+        <section class="menu-content">
+                        <a href="booking.php?id=1">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="assets/img/cars/audi-a4.jpg" alt="Card image cap">
+            <h5><b> Audi A4 </b></h5>
+            <h6> AC Fare: Rs. 36/km & Rs.5200/day</h6>
+            <h6> Non-AC Fare: Rs. 26/km & Rs.2600/day</h6>
+
+            
+            </div> 
+            </a>
+                        <a href="booking.php?id=2">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="assets/img/cars/creta.jpg" alt="Card image cap">
+            <h5><b> Hyundai Creta </b></h5>
+            <h6> AC Fare: Rs. 22/km & Rs.2900/day</h6>
+            <h6> Non-AC Fare: Rs. 12/km & Rs.1400/day</h6>
+
+            
+            </div> 
+            </a>
+                        <a href="booking.php?id=3">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="assets/img/cars/bmw6.jpg" alt="Card image cap">
+            <h5><b> BMW 6-Series </b></h5>
+            <h6> AC Fare: Rs. 39/km & Rs.6950/day</h6>
+            <h6> Non-AC Fare: Rs. 30/km & Rs.5999/day</h6>
+
+            
+            </div> 
+            </a>
+                        <a href="booking.php?id=4">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="assets/img/cars/mcec.jpg" alt="Card image cap">
+            <h5><b> Mercedes-Benz E-Class </b></h5>
+            <h6> AC Fare: Rs. 45/km & Rs.7200/day</h6>
+            <h6> Non-AC Fare: Rs. 30/km & Rs.5200/day</h6>
+
+            
+            </div> 
+            </a>
+                        <a href="booking.php?id=6">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="assets/img/cars/ecosport.png" alt="Card image cap">
+            <h5><b> Ford EcoSport </b></h5>
+            <h6> AC Fare: Rs. 21/km & Rs.3890/day</h6>
+            <h6> Non-AC Fare: Rs. 13/km & Rs.2600/day</h6>
+
+            
+            </div> 
+            </a>
+                        <a href="booking.php?id=8">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="assets/img/cars/rangero.jpg" alt="Card image cap">
+            <h5><b> Range Rover Sport </b></h5>
+            <h6> AC Fare: Rs. 36/km & Rs.6000/day</h6>
+            <h6> Non-AC Fare: Rs. 26/km & Rs.4600/day</h6>
+
+            
+            </div> 
+            </a>
+                        <a href="booking.php?id=9">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="assets/img/cars/mghector.jpg" alt="Card image cap">
+            <h5><b> MG Hector </b></h5>
+            <h6> AC Fare: Rs. 20/km & Rs.2900/day</h6>
+            <h6> Non-AC Fare: Rs. 12/km & Rs.1400/day</h6>
+
+            
+            </div> 
+            </a>
+                        <a href="booking.php?id=10">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="assets/img/cars/hondacr.jpg" alt="Card image cap">
+            <h5><b> Honda CR-V </b></h5>
+            <h6> AC Fare: Rs. 22/km & Rs.2850/day</h6>
+            <h6> Non-AC Fare: Rs. 15/km & Rs.1400/day</h6>
+
+            
+            </div> 
+            </a>
+                        <a href="booking.php?id=11">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="assets/img/cars/Mahindra XUV.jpg" alt="Card image cap">
+            <h5><b> Mahindra XUV 500 </b></h5>
+            <h6> AC Fare: Rs. 15/km & Rs.3000/day</h6>
+            <h6> Non-AC Fare: Rs. 13/km & Rs.2600/day</h6>
+
+            
+            </div> 
+            </a>
+                        <a href="booking.php?id=12">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="assets/img/cars/Fortuner.png" alt="Card image cap">
+            <h5><b> Toyota Fortuner </b></h5>
+            <h6> AC Fare: Rs. 16/km & Rs.3200/day</h6>
+            <h6> Non-AC Fare: Rs. 14/km & Rs.2800/day</h6>
+
+            
+            </div> 
+            </a>
+                        <a href="booking.php?id=13">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="assets/img/cars/hyundai0.png" alt="Card image cap">
+            <h5><b> Hyundai Veloster </b></h5>
+            <h6> AC Fare: Rs. 23/km & Rs.4500/day</h6>
+            <h6> Non-AC Fare: Rs. 15/km & Rs.3500/day</h6>
+
+            
+            </div> 
+            </a>
+                        <a href="booking.php?id=14">
+            <div class="sub-menu">
+            
+
+            <img class="card-img-top" src="assets/img/cars/jaguarxf.jpg" alt="Card image cap">
+            <h5><b> Jaguar XF </b></h5>
+            <h6> AC Fare: Rs. 39/km & Rs.6100/day</h6>
+            <h6> Non-AC Fare: Rs. 29/km & Rs.4380/day</h6>
+
+            
+            </div> 
+            </a>
+                                               
+        </section>
+                    
+    </div>
+
+   
+    <section class="why" id="aboutus">
+        <h3>Why choose AutoRevive?</h3>
+        <p>Autorevive was founded with the mission to revive the car rental experience. 
+        We prioritize customer satisfaction by offering a smooth rental process, clear communication, and competitive rates on a diverse fleet of vehicles.
+        Our vision is to become the leading car rental company, known for innovation and exceptional service.  We value transparency, reliability, and keeping our customers at the forefront of everything we do.  Whether you're looking for a convenient pick-up location, 24/7 support, or the latest car models, Autorevive is here to get you on the road.</p>
+        <p>We go beyond just rentals. We strive to be your trusted partner, ensuring a seamless and enjoyable experience from the moment you book your car through our user-friendly mobile app to the moment you return it.  Safety is paramount, so we offer well-maintained vehicles and transparent insurance options for peace of mind.  
+        Autorevive is also committed to giving back to the community, and we're proud to support clients for climate action.</p>
+        <p><b>Customer Testimonial :</b>  "Renting a car with Autorevive was a breeze! The website made booking and pick-up a snap, and the customer service team was incredibly helpful. The car was clean and perfect for my road trip. I'll definitely be using Autorevive again!" - Ashok Kumar.
+            Choose Autorevive for a car rental experience that's transparent, reliable, and puts you first.  Get where you need to be with ease, comfort, and confidence.  
+            Let Autorevive be your guide. Book your next rental and rediscover the joy of car rentals.</p>
+        </section>
+    
+    <footer class="site-footer">
+        <div class="container">
+            <hr>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h5><b>AutoRevive | All Rights Reserved | 2024 Copyrights ©</b></h5>
+                </div>
+                
+            </div>
+        </div>
+    </footer>
+    <script>
+        function myMap() {
+            myCenter = new google.maps.LatLng(25.614744, 85.128489);
+            var mapOptions = {
+                center: myCenter,
+                zoom: 12,
+                scrollwheel: true,
+                draggable: true,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+
+            var marker = new google.maps.Marker({
+                position: myCenter,
+            });
+            marker.setMap(map);
+        }
+    </script>
+    <script>
+        function sendGaEvent(category, action, label) {
+            ga('send', {
+                hitType: 'event',
+                eventCategory: category,
+                eventAction: action,
+                eventLabel: label
+            });
+        };
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCCuoe93lQkgRaC7FB8fMOr_g1dmMRwKng&callback=myMap" type="text/javascript"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+    <script src="assets/js/jquery.easing.min.js"></script>
+
+    <script src="assets/js/theme.js"></script>
+</body>
+
+</html>
